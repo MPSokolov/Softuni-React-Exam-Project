50e6df95f3886bb593aa30e4dbe08b7f25d4c8e7
MARTINPC\marti <martin_sokolov@yahoo.com>
2023-12-08 18:24:22 +0200

    Added text when no such recipe exists

--- MartinsRecipies/client/src/components/RecipeDetails.jsx
+++ MartinsRecipies/client/src/components/RecipeDetails.jsx
@@ -5,7 +5,7 @@ import * as recipeService from "../services/recipeService";
 import * as commentService from "../services/commentService";
 import AuthContext from "../contexts/authContext";
 
-import Button from "react-bootstrap/Button";
+import { Button, Alert } from 'react-bootstrap';
 import styles from "./assets/RecipeDetails.module.css";
 
 export default function RecipeDetails() {
@@ -14,6 +14,7 @@ export default function RecipeDetails() {
   const [recipeDetails, setRecipeDetails] = useState(null);
   const [commentText, setCommentText] = useState("");
   const [comments, setComments] = useState([]);
+  const [error, setError] = useState("");
 
   useEffect(() => {
     // Fetch recipe details when the component mounts
@@ -26,6 +27,9 @@ export default function RecipeDetails() {
         setComments(recipeComments);
       } catch (error) {
         console.error("Error fetching recipe details:", error);
+        setError(error.message);
+        // setError('');
+        // return
       }
     };
 
@@ -69,66 +73,72 @@ export default function RecipeDetails() {
   };
 
   return (
-    <div className={styles.recipeDetailsContainer}>
-      {recipeDetails ? (
-        <div className={styles.recipeCard}>
-          <h2>{recipeDetails.title}</h2>
-          <p>
-            Created on:{" "}
-            {new Date(
-              parseInt(recipeDetails._createdOn, 10)
-            ).toLocaleDateString()}
-          </p>
-          <img
-            src={recipeDetails.pictureUrl}
-            alt="Image"
-            className={styles.recipeImage}
-          />
-          <h3>Ingredients</h3>
-          <p>{recipeDetails.ingredients}</p>
-          <h3>Instructions</h3>
-          <p>{recipeDetails.instructions}</p>
-          {renderEditDeleteButtons()}
-          {/* Comment Form */}
-          {isAuthenticated && (
-            <form onSubmit={handleCommentSubmit} className={styles.commentForm}>
-              <label>
-                Add a Comment:
-                <textarea
-                  value={commentText}
-                  onChange={(e) => setCommentText(e.target.value)}
-                ></textarea>
-              </label>
-              {/* <button type="submit">Submit Comment</button> */}
-              <Button variant="primary" type="submit">
-                Submit Comment
-              </Button>
-            </form>
-          )}
-          {/* Display Comments */}
-          <div>
-            <h3 className={styles.commentsTitle}>Comments</h3>
-            {comments.map((comment) => (
-              <>
-                <div key={comment._id}>
-                  <p>
-                    <strong>{comment.username}:</strong> {comment.text}
-                  </p>
-                  <p>
-                    Posted on:{" "}
-                    {new Date(
-                      parseInt(comment._createdOn, 10)
-                    ).toLocaleDateString()}
-                  </p>
-                </div>
-                <hr />
-              </>
-            ))}
+    <>
+      {error && <Alert variant="danger">{error}</Alert>}
+      <div className={styles.recipeDetailsContainer}>
+        {recipeDetails ? (
+          <div className={styles.recipeCard}>
+            <h2>{recipeDetails.title}</h2>
+            <p>
+              Created on:{" "}
+              {new Date(
+                parseInt(recipeDetails._createdOn, 10)
+              ).toLocaleDateString()}
+            </p>
+            <img
+              src={recipeDetails.pictureUrl}
+              alt="Image"
+              className={styles.recipeImage}
+            />
+            <h3>Ingredients</h3>
+            <p>{recipeDetails.ingredients}</p>
+            <h3>Instructions</h3>
+            <p>{recipeDetails.instructions}</p>
+            {renderEditDeleteButtons()}
+            {/* Comment Form */}
+            {isAuthenticated && (
+              <form
+                onSubmit={handleCommentSubmit}
+                className={styles.commentForm}
+              >
+                <label>
+                  Add a Comment:
+                  <textarea
+                    value={commentText}
+                    onChange={(e) => setCommentText(e.target.value)}
+                  ></textarea>
+                </label>
+                {/* <button type="submit">Submit Comment</button> */}
+                <Button variant="primary" type="submit">
+                  Submit Comment
+                </Button>
+              </form>
+            )}
+            {/* Display Comments */}
+            <div>
+              <h3 className={styles.commentsTitle}>Comments</h3>
+              {comments.map((comment) => (
+                <>
+                  <div key={comment._id}>
+                    <p>
+                      <strong>{comment.username}:</strong> {comment.text}
+                    </p>
+                    <p>
+                      Posted on:{" "}
+                      {new Date(
+                        parseInt(comment._createdOn, 10)
+                      ).toLocaleDateString()}
+                    </p>
+                  </div>
+                  <hr />
+                </>
+              ))}
+            </div>
           </div>
-        </div>
-      ) : (
-        <p>Loading recipe details...</p>
-      )}
-    </div>
+        ) : (
+          <p>Loading recipe details...</p>
+        )}
+      </div>
+    </>
   );
 }
