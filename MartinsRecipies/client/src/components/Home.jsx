b68ceb6e9e24e16b8d2954145fc4d75f84139353
MARTINPC\marti <martin_sokolov@yahoo.com>
2023-12-08 10:28:00 +0200

    Added more info in home page

--- MartinsRecipies/client/src/components/Home.jsx
+++ MartinsRecipies/client/src/components/Home.jsx
@@ -3,7 +3,8 @@ import { useState, useEffect } from "react";
 import { get3MostRecent } from "../services/recipeService";
 
 import RecipeCard from "./RecipeCard";
-import styles from './assets/Home.module.css';
+import styles from "./assets/Home.module.css";
+import { Container } from "react-bootstrap";
 
 export default function Home() {
   const [recentRecipes, setRecentRecipes] = useState([]);
@@ -16,7 +17,7 @@ export default function Home() {
         const recipes = await get3MostRecent();
         setRecentRecipes(recipes);
       } catch (error) {
-        console.error('Error fetching recent recipes:', error);
+        console.error("Error fetching recent recipes:", error);
       }
     };
 
@@ -28,10 +29,32 @@ export default function Home() {
       <h2>Welcome to My Recipe App!</h2>
       <p>Discover and share delicious recipes created by this community.</p>
 
+      <Container className="mt-5">
+        <div>
+          <p>
+            Welcome to my Recipe App! We are passionate about sharing and
+            discovering delicious recipes. Our community is dedicated to
+            exploring the world of cooking and making delightful dishes.
+          </p>
+          <p>
+            Whether you're a seasoned chef or a cooking enthusiast, our platform
+            provides a space for you to share your favorite recipes and connect
+            with others who share the same culinary interests.
+          </p>
+          <p>
+            Join us on this culinary journey, explore amazing recipes, and be
+            part of a vibrant cooking community. Happy cooking!
+          </p>
+        </div>
+      </Container>
+      <br />
+
       <h3>Most Recent Recipes</h3>
       {recentRecipes.length > 0 ? (
         <div className={styles.cardHolder}>
-          {recentRecipes.map(recipe => <RecipeCard key={recipe._id} {...recipe} />)}
+          {recentRecipes.map((recipe) => (
+            <RecipeCard key={recipe._id} {...recipe} />
+          ))}
         </div>
       ) : (
         <p>No recent recipes available.</p>
